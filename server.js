99041cf4b74458048df9662d8b6ae53a783c6626
Mae Kindler <mae.akie.kindler@gmail.com>
2020-12-17 20:50:34 -0600

    Set up dependencies and Express application.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
