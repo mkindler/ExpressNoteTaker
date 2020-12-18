6d2f28d10f8f33f1b97496e346120e91e77f8b70
Mae Kindler <mae.akie.kindler@gmail.com>
2020-12-17 20:59:40 -0600

    Updated require pathway for HTML and API routes. Currently getting an error when testing application and trying to determine correct file naming structure.

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('./'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("../apiRoutes")(app);
+require("../htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
